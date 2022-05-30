5ba0c2a57c6cff40f1572b9a46e8e2745df9f67d
Manu <manuel.diaz@fundacionpiensa.cl>
2022-05-30 14:48:13 -0400

    Proyecto calidad de Vida

--- Proyecto.R
+++ Proyecto.R
@@ -1,10 +1,7 @@
 library(tidyverse)
 library(janitor)
 library(rio)
-
-#ECV2020 <- rio::import("C:/R/Introduccion a R diplomado/Proyecto Diplomado/Calidad de vida 2020/BBDD Indice ECV 2020.dta")
-
-#head(ECV2020)
+library(skimr)
 
 ECV2021 <- rio::import("Calidad de vida 2021/BBDD Opinion Politica EOP 2021.dta")
 ECV2019 <- rio::import("Calidad de Vida 2019/BBDD Calidad de vida ECV 2019.dta")
@@ -12,8 +9,6 @@ ECV2018 <- rio::import("Calidad de vida 2018/BBDD Calidad de vida 2018.dta")
 
 head(ECV2021)
 
-
-
-
-
+skimr::skim(ECV2021)
+skimr::skim(ECV2018)
 
