3790d0913cddcfb391c9a8375070cc08c01462d4
Manu <manuel.diaz@fundacionpiensa.cl>
2022-05-30 14:46:13 -0400

    nuevo

--- Proyecto.R
+++ Proyecto.R
@@ -11,17 +11,8 @@ ECV2019 <- rio::import("Calidad de Vida 2019/BBDD Calidad de vida ECV 2019.dta")
 ECV2018 <- rio::import("Calidad de vida 2018/BBDD Calidad de vida 2018.dta")
 
 head(ECV2021)
-# move to the project directory
-cd Projects/website
 
-# initiate the upstream tracking of the project on the GitHub repo
-git remote add origin https://github.com/manu1days/Proyecto-calidad-de-vida.git
 
-# pull all files from the GitHub repo (typically just readme, license, gitignore)
-git pull origin master
-
-# set up GitHub repo to track changes on local machine
-git push -u origin master
 
 
 
